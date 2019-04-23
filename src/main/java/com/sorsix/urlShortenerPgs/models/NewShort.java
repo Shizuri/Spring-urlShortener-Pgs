0b2ae38b3398cdbcb3c57ac5dfce53ad5cdd9242
Zdravko Mavkov <mavkov@hotmail.com>
2019-04-23 18:13:38 +0200

    removed @Range from NewShort

--- src/main/java/com/sorsix/urlShortenerPgs/models/NewShort.java
+++ src/main/java/com/sorsix/urlShortenerPgs/models/NewShort.java
@@ -1,9 +1,6 @@
 package com.sorsix.urlShortenerPgs.models;
 
-import org.hibernate.validator.constraints.Range;
-
 public final class NewShort {
 
-    @Range(min = 0, max = 999999999)
     public String newShortUrl;
 }
